375bd08af1c5d1b478b9edf4ea8634a69456feae
Julia <mjulia1794@gmail.com>
2021-04-04 19:41:22 +0300

    added header & 3 section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+ /* этот код помечает картинки, для удобства разработки */
+ let i = 1;
+ for(let li of gallery__carousel.querySelectorAll('.gallery__carousel-image')) {
+   li.style.position = 'relative';
+   li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0;font-size:0">${i}</span>`);
+   i++;
+ }
+
+ /* конфигурация */
+ let width = 305; // ширина картинки
+ let count = 3; // видимое количество изображений
+
+ let list = gallery__carousel.querySelector('.gallery__carousel-images');
+ let listElems = gallery__carousel.querySelectorAll('.gallery__carousel-image');
+
+ let position = 0; // положение ленты прокрутки
+
+ gallery__carousel.querySelector('.gallery__button-prev').onclick = function() {
+   // сдвиг влево
+   position += width * count;
+   // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+   position = Math.min(position, 0)
+   list.style.marginLeft = position + 'px';
+ };
+
+ gallery__carousel.querySelector('.gallery__button-next').onclick = function() {
+   // сдвиг вправо
+   position -= width * count;
+   // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+   position = Math.max(position, -width * (listElems.length - count));
+   list.style.marginLeft = position + 'px';
+ };
